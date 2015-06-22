a3790efffdb75133340c356fabdc9ffd7b38bef1
josuebrunel <josuebrunel@gmail.com>
2015-06-22 22:51:26 +0200

    script to run tests added

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+if [ ! -z $1 ]; then
+    TestCase=".${1}"
+else
+    TestCase=''
+fi 
+
+if [ ! -z $2 ]; then
+    Test=".${2}"
+else
+    Test=''
+fi
+
+python -m unittest tests$TestCase$Test
+
+

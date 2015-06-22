d3b5ec4dc19c4700f386c309338ffa368868a459
josuebrunel <josuebrunel@gmail.com>
2015-06-22 22:56:55 +0200

    get_games_info and get_league kind ok

---
+++ fantasy_sports/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import
+
+from fantasy_sports.fantasy_sports import FantasySport
---
+++ fantasy_sports/fantasy_sports.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+
+
+class FantasySport(object):
+    """FantasySport Class
+    """
+
+    url = 'http://fantasysports.yahooapis.com/fantasy/v2/'
+
+    def __init__(self, oauth, resource=None):
+        """Initialize a FantasySport object
+        """
+        self.oauth = oauth
+
+
+    def __repr__(self,):
+        return ""
+
+    def _get(self, uri):
+        """
+        """
+        if not self.oauth.token_is_valid():
+            self.oauth.refresh_access_token
+
+        response = self.oauth.session.get(uri)
+
+        return response
+
+    def get_game_info(self, game):
+        """Return game info
+        >>> yfs.get_game_info('mlb')
+        """
+        uri = 'games;games_keys={0}'.format(game)
+
+        response = self._get(uri)
+
+        return response
+
+    def get_league(self, league_keys):
+        """Return league data
+        >>> yfs.get_league(['league_key'])
+        """     
+        uri = 'leagues;league_keys={0}'.format(league_keys)
+
+        response = self._get(uri)
+
+        return response
+

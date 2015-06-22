9de27ae28c0a8d79e15e46e48d200e86f44c348f
josuebrunel <josuebrunel@gmail.com>
2015-06-22 22:51:41 +0200

    test added

---
+++ tests.py
@@ -0,0 +1,20 @@
+import pdb
+import unittest
+
+from yahoo_oauth import OAuth2
+
+from fantasy_sports import FantasySport
+
+class TestFantasySports(unittest.TestCase):
+
+    def setUp(self,):
+        oauth = OAuth2(None, None, from_file='oauth.json',base_url='http://fantasysports.yahooapis.com/fantasy/v2/')
+        self.yfs = FantasySport(oauth)
+
+    def test_get_game_info(self,):
+        response = self.yfs.get_game_info('mlb')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_league(self):
+        response = self.yfs.get_league('238.l.627060')
+        self.assertEqual(response.status_code, 200)

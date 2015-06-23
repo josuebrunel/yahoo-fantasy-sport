9b4d61c911cea55db2ec500fac760f2acbd00043
josuebrunel <josuebrunel@gmail.com>
2015-06-23 14:28:11 +0200

    fix #3 #4 #6 : tests OK

--- tests.py
+++ tests.py
@@ -1,18 +1,18 @@
 import pdb
 import unittest
 
-from yahoo_oauth import OAuth2
+from yahoo_oauth import OAuth2, OAuth1
 
-from fantasy_sports import FantasySport
+from fantasy_sport import FantasySport
 
-class TestFantasySports(unittest.TestCase):
+class TestFantasySport(unittest.TestCase):
 
     def setUp(self,):
-        oauth = OAuth2(None, None, from_file='oauth.json',base_url='http://fantasysports.yahooapis.com/fantasy/v2/')
+        oauth = OAuth1(None, None, from_file='oauth.json',base_url='http://fantasysports.yahooapis.com/fantasy/v2/')
         self.yfs = FantasySport(oauth)
 
     def test_get_games_info(self,):
-        response = self.yfs.get_games_info('mlb')
+        response = self.yfs.get_games_info('nfl')
         self.assertEqual(response.status_code, 200)
         
     def test_get_games_info_with_login(self,):

18cf8f28e37c09f024480532de9562f38431e102
Emil Maric <emil.maric@hotmail.com>
2015-12-20 02:37:54 -0800

    Fixed code smells arising from unused imports

--- yahoo_fantasy_sports/__init__.py
+++ yahoo_fantasy_sports/__init__.py
@@ -5,5 +5,3 @@ from .resource import Resource
 from .collection import Collection
 from .game import GamesFactory
 from .yahoo_fantasy_sports import YahooFantasySports
-# from fantasy_sport.roster import Player, Roster 
-# from fantasy_sport import utils
